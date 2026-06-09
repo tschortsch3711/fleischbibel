21712f20b0a56ddc54d35a31a75a62d7783b2a6c
tschortsch3711 <51137595+tschortsch3711@users.noreply.github.com>
2026-06-09 02:59:49 +0200

    Fix footer legal links

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -21,90 +21,22 @@ export default defineConfig({
       { text: 'Links', link: '/links' }
     ],
     sidebar: [
-      {
-        text: 'Schnellstart',
-        items: [
-          { text: '2-Minuten-Guide', link: '/schnellstart' }
-        ]
-      },
-      {
-        text: 'Die Fleischgemeinschaft',
-        items: [
-          { text: 'Überblick', link: '/gemeinschaft/' },
-          { text: 'Selbstverständnis', link: '/gemeinschaft/selbstverstaendnis' },
-          { text: 'Aufnahmebedingungen', link: '/gemeinschaft/aufnahmebedingungen' }
-        ]
-      },
-      {
-        text: 'Regeln',
-        items: [
-          { text: 'Überblick', link: '/regeln/' },
-          { text: 'Grundregeln', link: '/regeln/grundregeln' },
-          { text: 'WhatsApp', link: '/regeln/whatsapp' },
-          { text: 'Zusammenkünfte', link: '/regeln/zusammenkuenfte' }
-        ]
-      },
-      {
-        text: 'Fleischmeldeamt',
-        items: [
-          { text: 'Überblick', link: '/fleischmeldeamt/' },
-          { text: 'Monatliche Abfrage', link: '/fleischmeldeamt/monatliche-abfrage' },
-          { text: 'Fleischversammlungen', link: '/fleischmeldeamt/fleischversammlungen' },
-          { text: 'Hauptfleischversammlung', link: '/fleischmeldeamt/hauptfleischversammlung' },
-          { text: 'Fragab', link: '/fleischmeldeamt/fragab' }
-        ]
-      },
-      {
-        text: 'Organisation',
-        items: [
-          { text: 'Überblick', link: '/organisation/' },
-          { text: 'Kein Oberhaupt', link: '/organisation/kein-oberhaupt' },
-          { text: 'Freiwillige Beiträge', link: '/organisation/rollen' },
-          { text: 'Entscheidungen', link: '/organisation/entscheidungen' }
-        ]
-      },
-      {
-        text: 'Rituale & Kultur',
-        items: [
-          { text: 'Überblick', link: '/kultur/' },
-          { text: 'Fleischhymne', link: '/kultur/fleischhymne' },
-          { text: 'Begriffe', link: '/kultur/begriffe' },
-          { text: 'Chronik', link: '/kultur/chronik' }
-        ]
-      },
-      {
-        text: 'Verwaltung',
-        items: [
-          { text: 'Links', link: '/links' },
-          { text: 'Hinweise', link: '/hinweise' },
-          {
-            text: 'Rechtliches',
-            collapsed: false,
-            items: [
-              { text: 'Impressum', link: '/impressum' },
-              { text: 'Datenschutz', link: '/datenschutz' }
-            ]
-          }
-        ]
-      }
+      { text: 'Schnellstart', items: [{ text: '2-Minuten-Guide', link: '/schnellstart' }] },
+      { text: 'Die Fleischgemeinschaft', items: [{ text: 'Überblick', link: '/gemeinschaft/' }, { text: 'Selbstverständnis', link: '/gemeinschaft/selbstverstaendnis' }, { text: 'Aufnahmebedingungen', link: '/gemeinschaft/aufnahmebedingungen' }] },
+      { text: 'Regeln', items: [{ text: 'Überblick', link: '/regeln/' }, { text: 'Grundregeln', link: '/regeln/grundregeln' }, { text: 'WhatsApp', link: '/regeln/whatsapp' }, { text: 'Zusammenkünfte', link: '/regeln/zusammenkuenfte' }] },
+      { text: 'Fleischmeldeamt', items: [{ text: 'Überblick', link: '/fleischmeldeamt/' }, { text: 'Monatliche Abfrage', link: '/fleischmeldeamt/monatliche-abfrage' }, { text: 'Fleischversammlungen', link: '/fleischmeldeamt/fleischversammlungen' }, { text: 'Hauptfleischversammlung', link: '/fleischmeldeamt/hauptfleischversammlung' }, { text: 'Fragab', link: '/fleischmeldeamt/fragab' }] },
+      { text: 'Organisation', items: [{ text: 'Überblick', link: '/organisation/' }, { text: 'Kein Oberhaupt', link: '/organisation/kein-oberhaupt' }, { text: 'Freiwillige Beiträge', link: '/organisation/rollen' }, { text: 'Entscheidungen', link: '/organisation/entscheidungen' }] },
+      { text: 'Rituale & Kultur', items: [{ text: 'Überblick', link: '/kultur/' }, { text: 'Fleischhymne', link: '/kultur/fleischhymne' }, { text: 'Begriffe', link: '/kultur/begriffe' }, { text: 'Chronik', link: '/kultur/chronik' }] },
+      { text: 'Verwaltung', items: [{ text: 'Links', link: '/links' }, { text: 'Hinweise', link: '/hinweise' }, { text: 'Rechtliches', items: [{ text: 'Impressum', link: '/impressum' }, { text: 'Datenschutz', link: '/datenschutz' }] }] }
     ],
-    search: {
-      provider: 'local'
-    },
-    socialLinks: [
-      { icon: 'github', link: 'https://github.com/tschortsch3711/fleischbibel' }
-    ],
+    search: { provider: 'local' },
+    socialLinks: [{ icon: 'github', link: 'https://github.com/tschortsch3711/fleischbibel' }],
     footer: {
-      message: 'Amtlich anmutend, gemeinschaftlich gepflegt. [Impressum](/impressum) | [Datenschutz](/datenschutz)',
-      copyright: 'Fleischbibel der privaten Fleischgemeinschaft'
+      message: 'Amtlich anmutend, gemeinschaftlich gepflegt.',
+      copyright: '<a href="/impressum">Impressum</a> | <a href="/datenschutz">Datenschutz</a><br>Fleischbibel der privaten Fleischgemeinschaft'
     },
     lastUpdatedText: 'Zuletzt aktualisiert',
-    outline: {
-      label: 'Auf dieser Seite'
-    },
-    docFooter: {
-      prev: 'Vorherige Seite',
-      next: 'Nächste Seite'
-    }
+    outline: { label: 'Auf dieser Seite' },
+    docFooter: { prev: 'Vorherige Seite', next: 'Nächste Seite' }
   }
 })
